db21c266a22c646a00a485d3eeecbd3fce244a49
Luke Adams <adamslc@whitman.edu>
2016-10-19 20:42:28 -0700

    added test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,10 @@ addprocs(4)
 using ProgressMeter
 using ParallelProgressMeter
 
-@parallelprogress for i in 1:10
+@parallelprogress for i in 1:50
+    sleep(0.1)
+end
+
+@parallelprogress 0.5 "Testing..." for i in 1:50
     sleep(0.1)
 end
